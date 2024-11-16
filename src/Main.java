9d403fe7a7bf5f33d824ec71a3ac6bf78bbebee5
anshul7z <anshul777.s@gmail.com>
2024-11-16 16:55:13 +0530

    removed hello world msg and added server started message in console

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
+        System.out.println("http Server started at port :"+9090);
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost",9090),0);
         server.createContext("/hello", new HelloHandler());
         server.start();
